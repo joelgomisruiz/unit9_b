9acb9a9bcc2982adb49b8249cd08453b49cc9d78
joel <joelgomisruiz@gmail.com>
2015-05-12 16:45:58 +0200

    test del metodo Calcular tasa completado

--- unit9_2/src/unit9_2/AlumnoTest.java
+++ unit9_2/src/unit9_2/AlumnoTest.java
@@ -14,5 +14,89 @@ public class AlumnoTest {
 		
 		assertEquals(2000.00f, new Alumno().calculaTasaMatricula(edad, numerosa, repetidor), 1e-8);
 	}
+	
+	@Test
+	public void testcalculaTasaMatriculaB() {
+		int edad = 24;
+		boolean numerosa = false;
+		boolean repetidor = false;
+		
+		assertEquals(500.00f, new Alumno().calculaTasaMatricula(edad, numerosa, repetidor), 1e-8);
+	}
+	
+	@Test
+	public void testcalculaTasaMatriculaC() {
+		int edad = 24;
+		boolean numerosa = true;
+		boolean repetidor = false;
+		
+		assertEquals(250.00f, new Alumno().calculaTasaMatricula(edad, numerosa, repetidor), 1e-8);
+	}
+	@Test
+	public void testcalculaTasaMatriculaD() {
+		int edad = 24;
+		boolean numerosa = true;
+		boolean repetidor = true;
+		
+		assertEquals(250.00f, new Alumno().calculaTasaMatricula(edad, numerosa, repetidor), 1e-8);
+	}
+	
+	@Test
+	public void testcalculaTasaMatriculaE() {
+		int edad = 26;
+		boolean numerosa = true;
+		boolean repetidor = true;
+		
+		assertEquals(250.00f, new Alumno().calculaTasaMatricula(edad, numerosa, repetidor), 1e-8);
+	}
+	@Test
+	public void testcalculaTasaMatriculaF() {
+		int edad = 26;
+		boolean numerosa = false;
+		boolean repetidor = true;
+		
+		assertEquals(500.00f, new Alumno().calculaTasaMatricula(edad, numerosa, repetidor), 1e-8);
+	}
+	@Test
+	public void testcalculaTasaMatriculaG() {
+		int edad = 26;
+		boolean numerosa = false;
+		boolean repetidor = false;
+		
+		assertEquals(500.00f, new Alumno().calculaTasaMatricula(edad, numerosa, repetidor), 1e-8);
+	}
+	@Test
+	public void testcalculaTasaMatriculaH() {
+		int edad = 26;
+		boolean numerosa = true;
+		boolean repetidor = false;
+		
+		assertEquals(250.00f, new Alumno().calculaTasaMatricula(edad, numerosa, repetidor), 1e-8);
+	}
+	@Test
+	public void testcalculaTasaMatriculaI() {
+		int edad = 65;
+		boolean numerosa = false;
+		boolean repetidor = false;
+		
+		assertEquals(250.00f, new Alumno().calculaTasaMatricula(edad, numerosa, repetidor), 1e-8);
+	}
+	@Test
+	public void testcalculaTasaMatriculaJ() {
+		int edad = 55;
+		boolean numerosa = false;
+		boolean repetidor = false;
+		
+		assertEquals(400.00f, new Alumno().calculaTasaMatricula(edad, numerosa, repetidor), 1e-8);
+	}
+	@Test
+	public void testcalculaTasaMatriculaK() {
+		int edad = 55;
+		boolean numerosa = true;
+		boolean repetidor = false;
+		
+		assertEquals(150.00f, new Alumno().calculaTasaMatricula(edad, numerosa, repetidor), 1e-8);
+	}
+
 
 }
